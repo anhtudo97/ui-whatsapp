47c95720bedcfeb5a60c15f8dd6f69ac3b9c7d7c
anhtudo97 <anhtudo97@gmail.com>
2021-06-21 23:33:23 +0700

    feat: styled

---
+++ pages/components/Sidebar/index.jsx
@@ -0,0 +1,40 @@
+import { IconButton } from '@material-ui/core';
+import ChatIcon from '@material-ui/icons/chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import SearchIcon from '@material-ui/icons/Search';
+
+import {
+  Container,
+  IconsContainer,
+  UserAvatar,
+  Header,
+  SidebarButton,
+  SearchInput,
+  Search,
+} from './styled';
+
+function Sidebar() {
+  return (
+    <Container>
+      <Header>
+        <UserAvatar />
+        <IconsContainer>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </IconsContainer>
+      </Header>
+      <Search>
+        <SearchIcon />
+        <SearchInput placeholder="Search in chats" />
+      </Search>
+      <SidebarButton>Start a new chat</SidebarButton>
+      {/* List of chats */}
+    </Container>
+  );
+}
+
+export default Sidebar;
