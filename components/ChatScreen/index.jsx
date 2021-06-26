f6c2f9ecf22acf0caf2800d08ccf2b19bf511e67
anhtudo97 <anhtudo97@gmail.com>
2021-06-27 00:11:10 +0700

    feat: add chat screen

--- components/ChatScreen/index.jsx
+++ components/ChatScreen/index.jsx
@@ -1,9 +1,56 @@
+import { Avatar, IconButton } from '@material-ui/core';
+import AttachFileIcon from '@material-ui/icons/AttachFile';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import { useRouter } from 'next/router';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollection } from 'react-firebase-hooks/firestore';
 import styled from 'styled-components';
+import { auth, db } from '../../lib/firebase';
+import Message from '../Message';
 
-const ChatScreen = () => {
+const ChatScreen = ({ chat, messages }) => {
+  const [user] = useAuthState(auth);
+  const router = useRouter();
+  const [messagesSnapshot] = useCollection(
+    db
+      .collection('chats')
+      .doc(router.query.id)
+      .collection('messages')
+      .orderBy('timestamp', 'asc'),
+  );
+
+  const showMessages = () => {
+    if (messagesSnapshot) {
+      return messagesSnapshot.docs.map((message) => (
+        <Message
+          key={chat.id}
+          user={message.data().user}
+          message={{
+            ...message.data(),
+            timestamp: message.data().timestamp?.toDate().getTime(),
+          }}
+        />
+      ));
+    }
+  };
   return (
     <Container>
-      <h1>123</h1>
+      <Header>
+        <Avatar />
+
+        <HeaderInformation>
+          <h3>Rec Email</h3>
+          <p>Last seen ...</p>
+        </HeaderInformation>
+        <HeaderIcons>
+          <IconButton>
+            <AttachFileIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </HeaderIcons>
+      </Header>
     </Container>
   );
 };
@@ -12,4 +59,8 @@ export default ChatScreen;
 
 const Container = styled.div``;
 
-const ChatContainer = styled.div``;
+const Header = styled.div``;
+
+const HeaderIcons = styled.div``;
+
+const HeaderInformation = styled.div``;
---
+++ components/Message/index.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const Message = ({ user, message }) => {
+  return <Container>message</Container>;
+};
+
+export default Message;
+
+const Container = styled.div``;
